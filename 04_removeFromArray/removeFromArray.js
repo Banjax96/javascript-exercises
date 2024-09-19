916b368d52bed5f3049326f4ff2d9cfb1cc13234
Banjax96 <markmilner96@gmail.com>
2024-09-19 15:42:48 -0400

    Add function to remove elements from array for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    for (let input of toRemove){
+        for (let i = 0; i < array.length; i++){
+            if (array[i] === input){
+                array.splice(i, 1);
+                i -= 1;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
