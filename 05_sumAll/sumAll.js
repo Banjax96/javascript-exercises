d00d21fb4a97aa13e386702997c8877ee0c39200
Banjax96 <markmilner96@gmail.com>
2024-09-19 15:51:07 -0400

    Add logic to sumAll 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    let sum = 0;
 
+    if (start >= 0 && stop >= 0 && Number.isInteger(start) && Number.isInteger(stop)) {
+        if (start < stop){
+            for (let i = start; i <= stop; i++){
+                sum += i;
+            }
+        }
+        else if (start > stop){
+            for (let i = stop; i <= start; i++){
+                sum += i;
+            }
+        }
+    }
+    else {
+        sum = "ERROR";
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
